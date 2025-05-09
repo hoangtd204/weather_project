0d4239d632dfa7233ca4624a101cb476fe58d906
hoangtd <hoangtd@mor.com.vn>
2025-05-09 14:41:20 +0700

    commit day 9/5

---
+++ Weather/Views/menu.py
@@ -0,0 +1,8 @@
+def display_menu():
+    print("\n ====== Weather Forecast Menu ======")
+    print("1. Check current weather forecast.")
+    print("2. Check weather for the next 5 days.")
+    print("3. Check history searching")
+    print("4. Check sunrise, sunset.")
+    print("5. Check alert ")
+    print("0. Exit")
---
+++ Weather/main.py
@@ -0,0 +1,23 @@
+from Views.menu import display_menu
+
+
+
+def main():
+    while True:
+        display_menu()
+        choice = input("Your choice: ")
+        if choice == '1':
+            pass
+        elif choice == '2':
+            pass
+        elif choice == '3':
+            pass
+        elif choice == '4':
+            pass
+        elif choice == '5':
+            pass
+        elif choice == '0':
+            break
+        else:
+            print("Invalidate choice.PLease try again!")
+main()
